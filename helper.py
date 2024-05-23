39491715266072d60416b2562d3a5c0fcf12082a
CodeByCradle <codebycradle@Laptop-von-Mei-Shin.fritz.box>
2024-05-23 13:01:59 +0200

    refactor my program to take files as input

--- helper.py
+++ helper.py
@@ -1,4 +1,3 @@
-
 def create_filename_from_link(web_link: str):
     """Create a filename from a given web link. Ok.... the function sounds so stupid...
 
@@ -8,3 +7,23 @@ def create_filename_from_link(web_link: str):
     filename = web_link.split("/")
     # usually web_link.split("/")[-1] should really work 
     return filename[-1] 
+
+def create_input_output_pairs(filename: str):
+    """Create a pair with input link and output file name
+
+    Args:
+        filename (str): a file with multiple links. 
+
+    Returns:
+        dict: a dictionary with a structure like {"filename": "links"}
+    """
+    with open(filename, "r") as input_file:
+        links = input_file.readlines()
+        print(links)
+        file_pairs = {}
+        for link in links:
+            link = link.strip("\n")
+            filename = create_filename_from_link(link)
+            file_pairs[filename] = link
+        return file_pairs
+    
--- main.py
+++ main.py
@@ -1,75 +1,88 @@
 import argparse
 import requests
-from pathlib import Path 
+from pathlib import Path
 
 from bs4 import BeautifulSoup as bs
+from lxml import etree  # try this one to work on the lang tag
 
-from helper import create_filename_from_link
+from helper import create_input_output_pairs
 
 
-# actually, I should make a class, or should I?? don't know.... ok. I need to make a class 
+# actually, I should make a class, or should I?? don't know.... ok. I need to make a class
+
 
 class WebCrawler:
-    def __init__(self, web_link:str, filename_stem:str):
-        self.web_link: str = web_link 
-        self.filename_stem: str = filename_stem
+    def __init__(self, web_link: list, filename_stem: list):
+        """ initiation.
 
-    def test(self):
-        # soon we will remove it
-        print(f"{self.web_link} and {self.filename_stem}")
-        
-    def get_material(self): 
-        """Download material from a given web page link and save it 
+        Args:
+            web_link (str): _description_
+            filename_stem (str): _description_
+        """
+        self.web_link: list = web_link
+        self.filename_stem: list = filename_stem
+
+    def get_material(self, data_dir: str):
+        """Download material from a given web page link and save it
 
         Returns:
             str: a file path
         """
+        print(f"Current web link is {self.web_link}")
         r = requests.get(self.web_link)
         if r.status_code == 200:
-            # if the web page link is valid/responding. Here, it is not so good to hard code the path ("Data") because we **might** change it in the future.
-            # TODO: find a solution for the data path. 
-            with open(f"Data/{self.filename_stem}", "w") as xml_file:
+            with open(f"{data_dir}/{self.filename_stem}", "w") as xml_file:
                 xml_file.write(r.text)
-            return f"Data/{self.filename_stem}"
-        return None
-    
-    def extract_by_tag(slef, data_path:str, tag: str):
-        """Extract the data according to a given tag from the given data path.
+            return f"{data_dir}/{self.filename_stem}"
+        else:
+            print(r.status_code)
+            # This is so stupid. It should work without this else!
+            return None
+
+    def extract_by_tag(slef, data_path: str, tag: str):
+        """Extract the data according to a given tag from the given data path. I decide to move the language tag recognition to later.
 
         Returns:
-            list: an array contains all the sentences. 
+            list: an array contains all the sentences.
         """
-        try: 
+        try:
             with open(data_path) as f:
-                data = f.read() 
-                bs_data = bs(data, "xml")
-                tag_data = bs_data.find_all(tag) 
-                #TODO: find only the sentence, do not find the form or translation under the word <W> tag
-                _text =  [sentence.text for sentence in tag_data if sentence.find_parent("S")]  
-                return _text
+                bs_data = bs(f.read(), "xml")
+                tag_data = bs_data.find_all("S")
+                total_text = []
+                for each_td in tag_data:
+                    middle_process = each_td.find_all(tag, recursive=False)
+                    total_text.extend([sentence.text for sentence in middle_process])
+                return total_text
         except Exception as e:
             print(f"error is {e}")
-            
 
     def write_data_to_file(self, input_array, output_filename):
+        """Write the form and translation data to file
+
+        Args:
+            input_array (list): The form or translation
+            output_filename (str): The output file name, the stem, not the .txt or something. 
+        """
         with open(output_filename, "w") as output:
             for each_sentence in input_array:
-                output.write(each_sentence+"\n")
+                output.write(each_sentence + "\n")
 
 
 if __name__ == "__main__":
-    # initiate the argument parser. I should move this to another place later. 
+    # TODO: find a better way to reduce this argument parser.
     parser = argparse.ArgumentParser()
-    parser.add_argument("link", help="Please give a webpage link") 
+    parser.add_argument("input_file", help="Please give a file contains multiple web links.")
+    # TODO: make link input as an option(?). A file with multiple links will be the main input
+    parser.add_argument("data_dir", help="Please give a directory to store the data")
     args = parser.parse_args()
-    #"https://cocoon.huma-num.fr/data/jacques/masters/crdo-JYA_DELUGE.xml"
-    filename = create_filename_from_link(args.link)
-    wc = WebCrawler(args.link, filename)
-    xml_file_path = wc.get_material()
-    for tag in ["FORM", "TRANSL"]:
-        the_text = wc.extract_by_tag(Path(xml_file_path).as_posix(), tag) 
-        print(the_text)
-    # # this should work, in theory....
-    # write_data_to_file(jap_fr_arrays[0], "jap_three_sisters.txt")
-    # write_data_to_file(jap_fr_arrays[1], "fr_three_sisters.txt")
-    # for idx, value in jap_fr_dictionary.items():
+    data_pairs = create_input_output_pairs(args.input_file)
+    data_dir = args.data_dir
+    for filename, link in data_pairs.items(): 
+        wc = WebCrawler(link, filename)
+        xml_file_path = wc.get_material(data_dir)
+        # if xml_file_path:
+        #     # I need to find a better way for this. But now it should do. 
+        #     for tag in ["FORM", "TRANSL"]:
+        #         the_text = wc.extract_by_tag(Path(xml_file_path).as_posix(), tag)
+        #         wc.write_data_to_file(the_text, f"{data_dir}/{tag.lower()}_{filename}.txt")
